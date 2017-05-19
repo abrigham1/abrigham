58d60e64fc03d11c41239cf6b2cc6429f3e4c6dc
Aaron Brigham <abrigham1@gmail.com>
2017-05-19 07:27:43 -0400

    More initial stuff

--- resources/views/components/MovieReviewForm.blade.php
+++ resources/views/components/MovieReviewForm.blade.php
@@ -1,8 +1,8 @@
+{{-- we're doing this as an inline template because we need access to our route() helper function --}}
 <movie-review-form inline-template>
     <div class="movieReviewSection">
-
         <form v-on:submit.prevent="submit" method="post"
-              action="{{ route('predict-review', [], false) }}">
+              action="{{ route('predict-review') }}">
             <transition name="fade">
                 <div v-cloak v-show="showError" class="alert alert-danger alert-dismissible">
                     <button v-on:click="closeAlert" type="button" class="close" aria-label="Close">
@@ -15,13 +15,14 @@
             <div v-bind:class="{ 'has-error': errors.has('review') }" class="form-group">
                 <label class="control-label" for="movieReviewTextarea">Movie Review</label>
                 <transition name="fade">
-                    <label v-cloak class="error control-label" v-show="errors.has('review')">@{{ errors.first('review') }}</label>
+                    <label v-cloak class="error control-label"
+                           v-show="errors.has('review')">@{{ errors.first('review') }}</label>
                 </transition>
                 <transition name="fade">
                     <label v-cloak class="results control-label" v-show="showThinking">Hmmm let me give me a second to
                         think...</label>
                 </transition>
-                <textarea id="review"  data-vv-delay="500" v-model="review" name="review"
+                <textarea id="review" v-validate="'required|min:15'" data-vv-delay="500" v-model="review" name="review"
                           class="form-control"
                           rows="10"></textarea>
             </div>
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -4,7 +4,7 @@
             <div class="container-fluid">
                 <!-- Brand and toggle get grouped for better mobile display -->
                 <div class="navbar-header">
-                    <a class="navbar-brand" href="{{ route('home', [], false) }}">AB</a>
+                    <a class="navbar-brand" href="{{ route('home') }}">AB</a>
                 </div>
 
                 <!-- Collect the nav links, forms, and other content for toggling -->
