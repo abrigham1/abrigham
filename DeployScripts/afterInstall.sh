40f2d78775e99b3a91ff3695e2a0f14b4859c140
Aaron Brigham <abrigham1@gmail.com>
2017-05-28 09:41:49 -0400

    adjusting deploy to use s3

--- DeployScripts/afterInstall.sh
+++ DeployScripts/afterInstall.sh
@@ -3,23 +3,5 @@
 # move to our application directory
 cd /var/www/sites/abrigham
 
-# run composer install this also runs php artisan optimize
-composer install --no-dev --optimize-autoloader --no-interaction
-
-# clear our caches and cache our routes and config
-php artisan cache:clear
-php artisan view:clear
-php artisan route:cache
-php artisan config:cache
-
-# install npm dependencies
-npm install
-
-# run production assets
-npm run prod
-
-blahtest
-
-# run any migrations necessary the application is down now
-# currently we have no database or migrations so this is commented out
-# php artisan migrate
+# this is where we would run migrations however we currently have none
+#php artisan migrate
