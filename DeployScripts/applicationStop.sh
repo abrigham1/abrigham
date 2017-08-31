83d6d7f2a2dd2e5aa7b8becf3a0d66e8bf126c08
Aaron Brigham <abrigham1@gmail.com>
2017-08-31 07:23:48 -0400

    fixing deployment scripts

--- DeployScripts/applicationStop.sh
+++ DeployScripts/applicationStop.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 
+# applicationStop on aws always runs on the previous version of your code before deployment of the new code occurs
+# take that into account when adding commands here because they will only be run on the deployment after they
+# are originally deployed
+
 # move to our application directory
 cd /var/www/sites/abrigham
 
 # put the application in maintenance mode
 php artisan down
+
+# clear cached (bootstrap/cache/) files have to do this now because when we deploy we may have
+# a new version of laravel that no longer agrees with them
+php artisan clear-compiled
+php artisan route:clear
+php artisan config:clear
