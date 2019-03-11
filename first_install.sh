fb184bcd83d00601854fbf14a05daf3ce8b46db2
Aaron Brigham <abrigham1@gmail.com>
2019-03-11 17:02:29 -0400

    More changes to gitignore

--- first_install.sh
+++ first_install.sh
@@ -9,15 +9,3 @@ fi
 if [ ! -f ./laradock/.env ]; then
     cp ./laradock/env-example ./laradock/.env
 fi
-
-# copy abrigham.conf.example to abrigham.conf if it doesn't exist for laradock
-# needed to serve the laravel site at abrigham.localhost
-if [ ! -f ./laradock/nginx/sites/abrigham.conf ]; then
-    cp ./laradock/nginx/sites/abrigham.conf.example ./laradock/nginx/sites/abrigham.conf
-fi
-
-# copy mlapi.conf.example to mlapi.conf if it doesn't exist for laradock
-# needed to serve the movie review classifier at mlapi.abrigham.localhost
-if [ ! -f ./laradock/nginx/sites/mlapi.conf ]; then
-    cp ./laradock/nginx/sites/mlapi.conf.example ./laradock/nginx/sites/mlapi.conf
-fi
