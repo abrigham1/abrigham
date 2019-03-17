f04e6bfd9ab67c9d8fa2c31af5691f17ec6aeb62
Aaron Brigham <abrigham1@gmail.com>
2019-03-17 17:53:03 -0400

    Refining scripts

--- DeployScripts/enterProductionMode.sh
+++ DeployScripts/enterProductionMode.sh
@@ -4,4 +4,4 @@
 cd `dirname "$(readlink -f "$0")"`/../laradock
 
 # install production
-docker-compose run -u laradock workspace bash -c "composer install --no-dev --optimize-autoloader --no-interaction --no-suggest --no-progress && npm install --quiet && npm run prod && php artisan clear-compiled && php artisan route:clear && php artisan config:clear && php artisan cache:clear && php artisan view:clear && php artisan route:cache"
+docker-compose run --rm -u laradock workspace bash -c "composer install --no-dev --optimize-autoloader --no-interaction --no-suggest --no-progress && npm install --quiet && npm run prod && php artisan clear-compiled && php artisan route:clear && php artisan config:clear && php artisan cache:clear && php artisan view:clear && php artisan route:cache"
