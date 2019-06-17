55ff259d6d76acb5e64a660767095c9bc62d7874
Aaron Brigham <abrigham1@gmail.com>
2019-06-17 06:35:07 -0400

    Debugging travis dusk tests

--- first_install.sh
+++ first_install.sh
@@ -3,9 +3,15 @@
 # copy .env.example to .env if it doesn't exist
 if [ ! -f ./.env ]; then
     cp ./.env.example ./.env
+    echo "Successfully copied .env.example to .env"
+else
+    echo ".env already exists nothing to copy"
 fi
 
 # copy env-example to .env if it doesn't exist for laradock
 if [ ! -f ./laradock/.env ]; then
     cp ./laradock/env-example ./laradock/.env
+    echo "Successfully copied ./laradock/env-example to ./laradock/.env"
+else
+    echo "./laradock/.env already exists nothing to copy"
 fi
