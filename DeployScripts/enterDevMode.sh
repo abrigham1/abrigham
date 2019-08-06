de5b2ad1afbf92e8668fa357c787d78d0b892ea1
Aaron Brigham <abrigham1@gmail.com>
2019-08-06 06:49:54 -0400

    finally got CD working

--- DeployScripts/enterDevMode.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# switch to laradock directory
-cd `dirname "$(readlink -f "$0")"`/../laradock/
-
-# install local
-docker-compose run --rm -u laradock workspace bash -c "composer install --no-interaction --no-progress --no-suggest && npm install --quiet && npm run dev && php artisan clear-compiled && php artisan route:clear && php artisan config:clear && php artisan cache:clear && php artisan view:clear"
--- DeployScripts/enterProductionMode.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# switch to laradock directory
-cd `dirname "$(readlink -f "$0")"`/../laradock
-
-# install production
-docker-compose run --rm -u laradock workspace bash -c "composer install --no-dev --optimize-autoloader --no-interaction --no-suggest --no-progress && npm install --quiet && npm run prod && php artisan clear-compiled && php artisan route:clear && php artisan config:clear && php artisan cache:clear && php artisan view:clear && php artisan route:cache"
